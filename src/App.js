9afabb2602268fe69da9860110c5547f07face3c
Hadi <hadi@Sefy.io>
2023-06-14 22:10:17 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import logo from './logo2.svg';
+import appleStore from './assets/appleStore.png';
+import playStore from './assets/playStore.png';
+import iphoneCap from './assets/iphone.png';
+import pixelCap from './assets/pixel.png';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const handleLogoClick = () => {
+        window.location.reload(); // Reloads the page when the logo is clicked
+    };
+
+    const handleIOSClick = () => {
+        window.location.reload(); // Reloads the page when the logo is clicked
+    };
+
+    const handleAndroidClick = () => {
+        window.location.reload(); // Reloads the page when the logo is clicked
+    };
+
+    return (
+        <div className="App">
+            <img src={logo} className="App-logo" alt="logo" onClick={handleLogoClick} />
+
+            <div className="Content-container">
+                <h1 className="Main-text">
+                    Introducing <span className="highlighted-text">Malaaz!</span><br /><br />
+                    The new way for buying, selling, and renting property in
+                    Algeria.
+                </h1>
+
+                <div className="Screen-caps-container">
+                    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                        <img src={iphoneCap} className="App-ScreenCap" alt="iphone" onClick={handleIOSClick} />
+                        <img src={appleStore} className="App-store" alt="appleStore" onClick={handleIOSClick} />
+                    </div>
+                    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                        <img src={pixelCap} className="App-ScreenCap" alt="pixel" onClick={handleAndroidClick} />
+                        <img src={playStore} className="App-store" alt="playStore" onClick={handleAndroidClick} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
