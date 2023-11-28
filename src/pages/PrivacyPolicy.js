d91ecc5c61a2f12068fde372a1f0bccdceff4452
Hadi <abdelhadiaggoune@gmail.com>
2023-11-28 15:59:54 +0000

    Update PrivacyPolicy.js

--- src/pages/PrivacyPolicy.js
+++ src/pages/PrivacyPolicy.js
@@ -5,7 +5,7 @@ const PrivacyPolicy = () => {
             <div className="App">
 
                     <h1 className="Main-Title">Privacy Policy</h1>
-                    <p className="Indented-paragraph">Last updated 18/06/2023</p>
+                    <p className="Indented-paragraph">Last updated 28/11/2023</p>
                     <p className="Indented-paragraph">Our responsibility:</p>
                     <p className="Indented-paragraph">• Malaaz is the ‘controller’ of your personal data. This means we decide how and why your data is processed any time you use our service. We go into this in more detail below.</p>
                     <p className="Indented-paragraph">Your responsibility:</p>
@@ -21,13 +21,31 @@ const PrivacyPolicy = () => {
                     <p className="Indented-paragraph">Contact details: We use this information to distinguish you from other users, and to contact you if we need to.</p>
                     <p className="Indented-paragraph">Account information: Your login details, preferences and properties you save in our service</p>
                     <p className="Indented-paragraph">Information about how you use our service: Understanding how you use our websites and apps helps us to improve our service.</p>
+
                     <h2 className="Sub-Title">Links to third party websites</h2>
                     <p className="Indented-paragraph">Our websites and apps contain links to other websites. This policy does not apply to those other websites. You should check the privacy policy of any other website before submitting your personal data there.</p>
+
                     <h2 className="Sub-Title">Vulnerability Disclosure Policy</h2>
                     <p className="Indented-paragraph">When it comes to our service we don't compromise on security. If you've noticed something that doesn't seem quite right with any of our platforms please let us know details of the vulnerability and how to reproduce it.</p>
                     <p className="Indented-paragraph">Although we welcome all reports of suspected vulnerabilities, we aren't able to respond to every email, and aren't offering rewards or bounties for any finding forwarded to us.</p>
                     <p className="Indented-paragraph">We expect professionalism from any security researchers and for them to avoid privacy or legal violations; these violations include (amongst others) not destroying or corrupting our data, not interrupting our operations, not targeting our staff, not performing any scans on our systems and not trying to access the account details of any other user.</p>
                     <p className="Indented-paragraph">If you have disclosed a valid unique bug we will of course pass this on to our teams internally to resolve, but will not be able to keep you updated on the progress of the report. Regardless of the outcome of your finding we would ask that disclosures are made directly to us, and that no public disclosure is made without our prior explicit consent.</p>
+
+                    <h2 className="Sub-Title">Data Sharing Disclosures</h2>
+
+                    <p className="Indented-paragraph">We may share your personal data with the following parties:</p>
+                    <p className="Indented-paragraph">Legal and Compliance: When required by law, regulation, or legal process, or to protect the rights, property, or safety of Malaaz, its users, or others, we may need to disclose your personal data to law enforcement, government authorities, or other third parties.</p>
+                    <p className="Indented-paragraph">Service Providers: Companies that provide services on our behalf, such as hosting, data analysis, payment processing, order fulfillment, infrastructure provision, IT services, customer service, email delivery services, credit card processing, and other similar services.</p>
+
+                    <h2 className="Sub-Title">Data Retention and Deletion Policy</h2>
+                    <p className="Indented-paragraph">Retention: We retain personal data for as long as necessary to fulfill the purposes for which it was collected, including for the purposes of satisfying any legal, regulatory, tax, accounting, or reporting requirements. The criteria used to determine our retention periods include the length of time we have an ongoing relationship with you and provide the services, whether there is a legal obligation to which we are subject, or whether retention is advisable in light of our legal position.</p>
+                    <p className="Indented-paragraph">Deletion: Upon your request, we will delete or anonymize your personal data so that it no longer identifies you, unless we are legally allowed or required to maintain certain personal data, including situations such as the following:</p>
+                    <p className="Indented-paragraph">If there is an unresolved issue relating to your account, such as an outstanding credit on your account or an unresolved claim or dispute, we will retain the necessary personal data until the issue is resolved.</p>
+                    <p className="Indented-paragraph">Where we are required to retain the personal data for our legal, tax, audit, and accounting obligations, we will retain the necessary personal data for the period required by applicable law.</p>
+
+                    <h2 className="Sub-Title">Privacy Contact Information</h2>
+                    <p className="Indented-paragraph">For any inquiries, concerns, or requests regarding our use of your personal data, please contact our dedicated Privacy Team at malaazdevelopment@gmail.com. We are committed to addressing your privacy-related issues promptly and transparently.</p>
+
             </div>
         );
 };
