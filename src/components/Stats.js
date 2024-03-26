6e47f36206c3c49067c9877bdc2c9a48475170a2
Hadi <malaazdevelopment@gmail.com>
2024-03-26 14:29:24 +0100

    v2.0.1

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -4,10 +4,13 @@ import './Stats.css';
 const Stats = () => {
     return (
         <div className="StatsBackground">
+
+            <span className="description-text">بوابتكم للعقارات التي تسعى لتبسيط وتحسين تجربتكم في شراء، بيع، وتأجير العقارات. بواسطة تطبيقنا، ستجدون منصة شاملة تجمع كافة الفاعلين في سوق العقارات من مشترين وبائعين ومستأجرين. تصفحوا مجموعة متنوعة من العقارات، اطلعوا على مقارنات مباشرة بيسر، واحصلوا على كل ما تحتاجونه من معلومات تفصيلية بما في ذلك الصور والجولات الافتراضية لضمان اتخاذكم للقرار الأمثل بكل راحة وثقة.</span>
+
             <div className="stats-container">
 
                 <div className="stat">
-                    <span className="highlighted-text"><strong>2,500+ </strong></span>
+                    <span className="highlighted-text"><strong>3,500+ </strong></span>
                     <span className="downloads">تحميل</span>
                 </div>
 
